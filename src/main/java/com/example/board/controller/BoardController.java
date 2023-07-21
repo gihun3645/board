05fe24df98b116d7e6709b1bb122b697c8dbb1b6
송바래(ばれ） <gihun3645@naver.com>
2023-07-21 16:25:51 +0900

    글쓰기, 목록보기 구현

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.example.board.controller;
 
+import com.example.board.dto.Board;
 import com.example.board.dto.LoginInfo;
+import com.example.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 // HTTP 요청을 받아서 응답을 하는 컴포넌트, 스프링 부트가 자동으로 Bean을 생성한다.
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
     // 게시물 목록을 보여준다.
     // 컨트롤러의 메소드가 리턴하는 문자열은 템플릿 이름이다.
     // localhost:8080  ---> "list"라는 이름의 템플릿 이름이다.
@@ -24,6 +31,27 @@ public class BoardController {
         model.addAttribute("loginInfo", loginInfo);
         // 게시물 목록을 불러온다
         // 페이징 처리한다
+        int page = 1;
+        int totalCount = boardService.getTotalCount();
+        List<Board> list = boardService.getBoards(page); // page가 1,2,3,4
+        System.out.println("totalCount : " + totalCount);
+
+//        for (Board board : list) {
+//            System.out.println(board);
+//        }
+
+        // 총 페이지 수 구하기
+        int pageCount = totalCount / 10;
+        if (totalCount % 10 > 0) { // 나머지가 있을 경우 1페이지 추가
+            pageCount += 1;
+        }
+        int currentPage = page; // 현재 페이지
+
+        // 템플릿 엔진에 전달
+        model.addAttribute("list", list);
+        model.addAttribute("pageCount", pageCount);
+        model.addAttribute("currentPage", currentPage);
+
         return "list";
     }
 
@@ -46,24 +74,33 @@ public class BoardController {
     // 수정한다 관리자는 모든 글을 수정할 수 있다.
 
     @GetMapping("/writeForm")
-    public String writeForm() {
+    public String writeForm(HttpSession session, Model model) {
         // 로그인한 사용자만 글을 써야한다. 로그인을 하지 않았다면 리스트보기로 자동 이동
         // 세션에서 로그인한 정보를 읽어들인다.
-
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("loginInfo");
+        if (loginInfo == null) { // 세션에 로그인 정보가 없다면 로그인 창으로 리다이렉트
+            return "redirect:/loginform";
+        }
+        model.addAttribute("loginInfo", loginInfo);
         return "writeForm";
     }
 
     @PostMapping("/write")
     public String write(
             @RequestParam("title") String title,
-            @RequestParam("content") String content
+            @RequestParam("content") String content,
+            HttpSession session
     ) {
-
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("loginInfo");
+        if (loginInfo == null) { // 세션에 로그인 정보가 없다면 로그인 창으로 리다이렉트
+            return "redirect:/loginform";
+        }
         // 로그인한 사용자만 글을 써야한다.
         // 세션에서 로그인한 정보를 읽어들인다. 로그인을 하지 않았다면 리스트보기로 자동 이동
         System.out.println("title : " + title);
         System.out.println("content : " + content);
 
+        boardService.addBoard(loginInfo.getUserId(), title, content);
 
         // 로그인한 회원 정보 + 제목, 내용을 저장한다.
 
---
+++ src/main/java/com/example/board/dao/BoardDao.java
@@ -0,0 +1,59 @@
+package com.example.board.dao;
+
+import com.example.board.dto.Board;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class BoardDao {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertBoard;
+
+    // 생성자 주입, 스프링이 자동으로 HikariCP Bean을 주입한다.
+    public BoardDao(DataSource dataSource) {
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        insertBoard = new SimpleJdbcInsert(dataSource)
+                .withCatalogName("exampledb")
+                .withTableName("board")
+                .usingGeneratedKeyColumns("board_id");
+    }
+
+    @Transactional
+    public void addBoard(int userId, String title, String content) {
+        Board board = new Board();
+        board.setUserId(userId);
+        board.setTitle(title);
+        board.setContent(content);
+        board.setRegDate(LocalDateTime.now());
+        SqlParameterSource params = new BeanPropertySqlParameterSource(board);
+        insertBoard.execute(params);
+    }
+
+    @Transactional(readOnly = true)
+    public int getTotalCount() {
+        String sql = "SELECT COUNT(*) as total_count FROM board"; // 무조건 1건의 데이터
+        Integer totalCount =  jdbcTemplate.queryForObject(sql, Map.of(), Integer.class);
+        return totalCount.intValue();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Board> getBoards(int page) {
+        // start 는 0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+        int start = (page - 1) * 10;
+        String sql= "select b.user_id, b.board_id, b.title, b.regdate, b.view_cnt, u.name from board b, user u where b.user_id = u.user_id  order by board_id desc limit :start, 10";
+        RowMapper<Board> rowMapper = BeanPropertyRowMapper.newInstance(Board.class);
+        List<Board> list = jdbcTemplate.query(sql, Map.of("start", start), rowMapper);
+        return list;
+    }
+}
